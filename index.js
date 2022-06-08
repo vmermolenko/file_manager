c1f8b5989ecd3fbe1cd46fde48a963d852965faf
Vadim <vmermolenko.dev@gmail.com>
2022-06-09 00:28:00 +0300

    add List of operations Basic operations system info Hash

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import os from 'os';
 import process from 'process';
 
-import { homedir, up, cd, ls } from './src/nav/navigation.js'
+import { getOSinfo } from './src/nav/navos.js'
+
+import { homedir, up, cd, ls, cat, add, rename, copyStream, remove, calculateHash } from './src/nav/navigation.js'
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,8 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 console.log(__filename);
-
 console.log(__dirname);
+
+let currentDir = homedir();
 
 
 /////////////
@@ -21,27 +24,78 @@ console.log(__dirname);
 //console.log(userHomeDir.split(path.sep).pop());
 //await list(userHomeDir)
 
-const userhomedir = await homedir()
+//const userhomedir = homedir()
 
-console.log(userhomedir);
-console.log(await up(userhomedir));
+//console.log(userhomedir);
+//console.log(up(userhomedir));
 
 ///////////
 
 const username = process.argv.slice(2)[0].split("=")[1];
 console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${currentDir}`);
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', async function() {
 	var input = process.stdin.read();
 
 	if (input !== null) {
 		//process.stdout.write(input);
 		var command = input.trim();
+
+		let nav = command.split(' ');
+
+		//console.log(nav);
+		/*
 		if (command == 'exit') {
-				process.exit(0);
+			process.exit(0);
 		}
+		*/
+		//console.log(nav[0]);
+		switch (nav[0]){
+			case 'exit' :
+				process.exit(0);
+			case 'up' :
+				currentDir = await up(currentDir);
+				break;
+			case 'cd' :
+				currentDir = await cd(currentDir, nav[1]);
+				break;
+			case 'ls' :
+				await ls(currentDir);
+				break;
+			case 'cat' :
+				await cat(currentDir, nav[1]); 
+				break;
+			case 'add' :
+				await add(currentDir, nav[1]); 
+				break;
+			case 'rn' :
+				await rename(currentDir, nav[1], nav[2]); 
+				break;
+			case 'cp' :
+				await copyStream(currentDir, nav[1], nav[2]); 
+				break;
+			case 'mv' :
+				await copyStream(currentDir, nav[1], nav[2]); 
+				await remove(currentDir, nav[1]);
+				break;
+			case 'rm' :
+				await remove(currentDir, nav[1]);
+				break;
+			case 'os':
+				await getOSinfo(nav[1]);
+				break;
+			case 'hash':
+				await calculateHash(currentDir, nav[1]);
+				break;
+			default:
+				console.log('Invalid input');
+				break;
+		} 
+		console.log(`You are currently in ${currentDir}`);	
+
 		process.stdin.resume();
 	}
 });
--- src/nav/navigation.js
+++ src/nav/navigation.js
@@ -1,17 +1,34 @@
 import os from 'os';
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto'
 
-const homedir = async () => {
+const homedir = () => {
 	return os.homedir();
 };
 
 const up = async (pathDir) => {
-    return pathDir.split(path.sep) 
+	if (homedir() === pathDir) {
+		return pathDir;
+	}
+	const arr = pathDir.split(path.sep)
+	arr.pop();
+    return arr.join(path.sep);
 };
 
-const cd = async (pathDir) => {
-    return pathDir.dirname(filename).split(path.sep).pop()   
+const cd = async (pathDir, pathDest) => {
+	if (fs.existsSync(pathDest) && isDir(pathDest)) {
+		return pathDest;   
+	}
+
+	const pathDestDir = path.join(pathDir, pathDest);
+	if (fs.existsSync(pathDestDir) && isDir(pathDestDir) && isExistHomedir(pathDestDir)) {
+		return pathDestDir;
+	}
+
+	console.log('Operation failed');
+	
+    return pathDir;
 };
 
 const ls = async (pathFolder) => {
@@ -21,5 +38,139 @@ const ls = async (pathFolder) => {
 	});
 };
 
+const cat = async (currentDir, pathFile) => {
+	let tmpPath = path.join(currentDir, pathFile);
 
-export { homedir, up, cd, ls }
+	if ((fs.existsSync(pathFile) && isExistHomedir(pathFile))) {
+		tmpPath = pathFile;
+	} else if ( !(fs.existsSync(tmpPath) && isExistHomedir(tmpPath)) ){
+		console.log('Operation failed')
+		return;
+	} 
+
+	fs.readFile(tmpPath, 'utf8', (err, data) => {
+		if (err) {
+			console.log('Operation failed');
+			return;
+		};
+		console.log(data);
+	});
+};
+
+const add = async (currentDir, new_file_name) => {
+	const tmpPath = path.join(currentDir, new_file_name);
+	if (fs.existsSync(tmpPath)) {
+		console.log('Operation failed1');
+	} else {
+		fs.writeFile(tmpPath, '', (err) => {
+			if (err) console.log('Operation failed2');				
+		});
+	}
+};
+
+const rename = async (currentDir, pathFile, new_filename) => {
+	let oldPath = path.join(currentDir, pathFile);
+
+	if ((fs.existsSync(pathFile) && isExistHomedir(pathFile))) {
+		oldPath = pathFile;
+	} else if ( !(fs.existsSync(oldPath) && isExistHomedir(oldPath)) ){
+		console.log('Operation failed')
+		return;
+	} 
+
+	const arrPath = oldPath.split(path.sep)
+	arrPath.pop();
+	arrPath.push(new_filename)
+    const newPath = arrPath.join(path.sep);
+
+	console.log(newPath);
+
+    if (!fs.existsSync(oldPath) || fs.existsSync(newPath)) {
+		console.log('Operation failed')
+		return;
+	}
+    fs.renameSync(oldPath, newPath);
+};
+
+
+const copyStream = async (currentDir, pathFile, path_to_new_directory) => {
+	
+	let oldPath = path.join(currentDir, pathFile);
+
+	if ((fs.existsSync(pathFile) && isExistHomedir(pathFile))) {
+		oldPath = pathFile;
+	} else if ( !(fs.existsSync(oldPath) && isExistHomedir(oldPath)) ){
+		console.log('Operation failed')
+		return;
+	} 
+
+	const fileName = oldPath.split(path.sep).pop();
+	const newPath = path.join(currentDir, path_to_new_directory,fileName);
+
+	if ((fs.existsSync(newPath) && isExistHomedir(newPath))) {
+		console.log('Operation failed')
+		return;
+	}
+
+	var readable = fs.createReadStream(oldPath, { encoding: 'utf8', highWaterMark: 16 * 1024 });
+	var writable = fs.createWriteStream(newPath);
+	
+	readable.pipe(writable);
+};
+
+const remove = async (currentDir, pathFile) => {
+	
+	let oldPath = path.join(currentDir, pathFile);
+
+	if ((fs.existsSync(pathFile) && isExistHomedir(pathFile))) {
+		oldPath = pathFile;
+	} else if ( !(fs.existsSync(oldPath) && isExistHomedir(oldPath)) ){
+		console.log('Operation failed')
+		return;
+	} 
+
+	fs.rm(oldPath, (err) => {
+		if (err) {
+			console.log('Operation failed')
+		};
+	})
+};
+
+const calculateHash = async (currentDir, pathFile) => {
+	let tmpPath = path.join(currentDir, pathFile);
+
+	if ((fs.existsSync(pathFile) && isExistHomedir(pathFile))) {
+		tmpPath = pathFile;
+	} else if ( !(fs.existsSync(tmpPath) && isExistHomedir(tmpPath)) ){
+		console.log('Operation failed')
+		return;
+	} 
+
+	const fileBuffer = fs.readFileSync(tmpPath);
+	const hashSum = crypto.createHash('sha256');
+	hashSum.update(fileBuffer);
+	const hex = hashSum.digest('hex');
+
+	console.log(hex);
+};
+
+
+function isDir(path) {
+    try {
+        var stat = fs.lstatSync(path);
+        return stat.isDirectory();
+    } catch (e) {
+        return false;
+    }
+}
+
+function isExistHomedir(pathDest) {
+    const pathFirst = homedir();
+	const tmp = pathDest.slice(0,pathFirst.length);
+	return pathFirst === tmp
+}
+
+
+
+
+export { homedir, up, cd, ls, cat, add, rename, copyStream, remove, calculateHash }
